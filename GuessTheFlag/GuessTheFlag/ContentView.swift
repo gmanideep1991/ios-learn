173e0691a654bfeab031ae37a7c750481af787b0
Manideep Gattamaneni <gmanideep1991@gmail.com>
2025-10-21 08:03:37 -0400

    Added gitignore and published guess the flag project

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Manideep Gattamaneni on 10/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            VStack{
+                Text("1")
+            }
+            VStack{
+                Text("2")
+            }
+            VStack{
+                Text("3")
+            }
+        }
+        HStack{
+            VStack{
+                Text("4")
+            }
+            VStack{
+                Text("5")
+            }
+            VStack{
+                Text("6")
+            }
+        }
+        HStack{
+            VStack{
+                Text("7")
+            }
+            VStack{
+                Text("8")
+            }
+            VStack{
+                Text("9")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Manideep Gattamaneni on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
