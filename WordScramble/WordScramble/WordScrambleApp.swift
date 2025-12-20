09c9d50392d6091fe76f26a77723ac31017a948a
Manideep Gattamaneni <gmanideep1991@gmail.com>
2025-12-20 12:24:37 -0500

    finished project 5(word scramble)

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Manideep Gattamaneni on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
